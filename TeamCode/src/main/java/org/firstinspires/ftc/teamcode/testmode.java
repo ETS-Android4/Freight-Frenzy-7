061f08a744d29ea4993f1e645c3e4f0a5716eb8e
thaylenme <88939080+thaylenme@users.noreply.github.com>
2021-09-26 14:56:14 -0400

    Changed leftDrive to LFDrive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testmode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testmode.java
@@ -56,7 +56,7 @@ public class testmode extends OpMode
 {
     // Declare OpMode members.
     private ElapsedTime runtime = new ElapsedTime();
-    private DcMotor leftDrive = null;
+    private DcMotor LFDrive = null;
     private DcMotor rightDrive = null;
 
     /*
@@ -69,12 +69,12 @@ public class testmode extends OpMode
         // Initialize the hardware variables. Note that the strings used here as parameters
         // to 'get' must correspond to the names assigned during the robot configuration
         // step (using the FTC Robot Controller app on the phone).
-        leftDrive  = hardwareMap.get(DcMotor.class, "left_drive");
+        LFDrive = hardwareMap.get(DcMotor.class, "left_drive");
         rightDrive = hardwareMap.get(DcMotor.class, "right_drive");
 
         // Most robots need the motor on one side to be reversed to drive forward
         // Reverse the motor that runs backwards when connected directly to the battery
-        leftDrive.setDirection(DcMotor.Direction.FORWARD);
+        LFDrive.setDirection(DcMotor.Direction.FORWARD);
         rightDrive.setDirection(DcMotor.Direction.REVERSE);
 
         // Tell the driver that initialization is complete.
@@ -121,7 +121,7 @@ public class testmode extends OpMode
         // rightPower = -gamepad1.right_stick_y ;
 
         // Send calculated power to wheels
-        leftDrive.setPower(leftPower);
+        LFDrive.setPower(leftPower);
         rightDrive.setPower(rightPower);
 
         // Show the elapsed game time and wheel power.
