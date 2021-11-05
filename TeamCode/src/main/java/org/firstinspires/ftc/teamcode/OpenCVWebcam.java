7ef7ed48d3826bfe4265e1ef3e476a594467a6e9
hopeshunta <hope.shunta@gmail.com>
2021-11-05 18:53:36 -0400

    worked on webcam -added pseudocode for 2square and 3square methods

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpenCVWebcam.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpenCVWebcam.java
@@ -36,89 +36,41 @@ import org.openftc.easyopencv.OpenCvCameraFactory;
 import org.openftc.easyopencv.OpenCvCameraRotation;
 import org.openftc.easyopencv.OpenCvPipeline;
 
-@TeleOp(name="WebcamExample", group="OpMode")
-public class OpenCVWebcamExample extends LinearOpMode
+@TeleOp(name="Webcam", group="OpMode")
+public class OpenCVWebcam extends LinearOpMode
 {
     OpenCvCamera webcam;
 
     @Override
     public void runOpMode()
     {
-        /*
-         * Instantiate an OpenCvCamera object for the camera we'll be using.
-         * In this sample, we're using a webcam. Note that you will need to
-         * make sure you have added the webcam to your configuration file and
-         * adjusted the name here to match what you named it in said config file.
-         *
-         * We pass it the view that we wish to use for camera monitor (on
-         * the RC phone). If no camera monitor is desired, use the alternate
-         * single-parameter constructor instead (commented out below)
-         */
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
 
-        // OR...  Do Not Activate the Camera Monitor View
-
-        //webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"));
-
-        /*
-         * Specify the image processing pipeline we wish to invoke upon receipt
-         * of a frame from the camera. Note that switching pipelines on-the-fly
-         * (while a streaming session is in flight) *IS* supported.
-         */
         webcam.setPipeline(new SamplePipeline());
 
-        /*
-         * Open the connection to the camera device. New in v1.4.0 is the ability
-         * to open the camera asynchronously, and this is now the recommended way
-         * to do it. The benefits of opening async include faster init time, and
-         * better behavior when pressing stop during init (i.e. less of a chance
-         * of tripping the stuck watchdog)
-         *
-         * If you really want to open synchronously, the old method is still available.
-         */
         webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
             @Override
             public void onOpened()
             {
-                /*
-                 * Tell the webcam to start streaming images to us! Note that you must make sure
-                 * the resolution you specify is supported by the camera. If it is not, an exception
-                 * will be thrown.
-                 *
-                 * Keep in mind that the SDK's UVC driver (what OpenCvWebcam uses under the hood) only
-                 * supports streaming from the webcam in the uncompressed YUV image format. This means
-                 * that the maximum resolution you can stream at and still get up to 30FPS is 480p (640x480).
-                 * Streaming at e.g. 720p will limit you to up to 10FPS and so on and so forth.
-                 *
-                 * Also, we specify the rotation that the webcam is used in. This is so that the image
-                 * from the camera sensor can be rotated such that it is always displayed with the image upright.
-                 * For a front facing camera, rotation is defined assuming the user is looking at the screen.
-                 * For a rear facing camera or a webcam, rotation is defined assuming the camera is facing
-                 * away from the user.
-                 */
                 webcam.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
             }
 
             @Override
             public void onError(int errorCode) {
-
             }
         });
 
         telemetry.addLine("Waiting for start");
         telemetry.update();
 
-        /*
-         * Wait for the user to press start on the Driver Station
-         */
         waitForStart();
 
         while (opModeIsActive())
         {
             /*
              * Send some stats to the telemetry
-             */
+
             telemetry.addData("Frame Count", webcam.getFrameCount());
             telemetry.addData("FPS", String.format("%.2f", webcam.getFps()));
             telemetry.addData("Total frame time ms", webcam.getTotalFrameTimeMs());
@@ -126,7 +78,7 @@ public class OpenCVWebcamExample extends LinearOpMode
             telemetry.addData("Overhead time ms", webcam.getOverheadTimeMs());
             telemetry.addData("Theoretical max FPS", webcam.getCurrentPipelineMaxFps());
             telemetry.update();
-
+*/
             /*
              * NOTE: stopping the stream from the camera early (before the end of the OpMode
              * when it will be automatically stopped for you) *IS* supported. The "if" statement
@@ -166,48 +118,13 @@ public class OpenCVWebcamExample extends LinearOpMode
         }
     }
 
-    /*
-     * An example image processing pipeline to be run upon receipt of each frame from the camera.
-     * Note that the processFrame() method is called serially from the frame worker thread -
-     * that is, a new camera frame will not come in while you're still processing a previous one.
-     * In other words, the processFrame() method will never be called multiple times simultaneously.
-     *
-     * However, the rendering of your processed image to the viewport is done in parallel to the
-     * frame worker thread. That is, the amount of time it takes to render the image to the
-     * viewport does NOT impact the amount of frames per second that your pipeline can process.
-     *
-     * IMPORTANT NOTE: this pipeline is NOT invoked on your OpMode thread. It is invoked on the
-     * frame worker thread. This should not be a problem in the vast majority of cases. However,
-     * if you're doing something weird where you do need it synchronized with your OpMode thread,
-     * then you will need to account for that accordingly.
-     */
     class SamplePipeline extends OpenCvPipeline
     {
         boolean viewportPaused;
 
-        /*
-         * NOTE: if you wish to use additional Mat objects in your processing pipeline, it is
-         * highly recommended to declare them here as instance variables and re-use them for
-         * each invocation of processFrame(), rather than declaring them as new local variables
-         * each time through processFrame(). This removes the danger of causing a memory leak
-         * by forgetting to call mat.release(), and it also reduces memory pressure by not
-         * constantly allocating and freeing large chunks of memory.
-         */
-
         @Override
         public Mat processFrame(Mat input)
         {
-            /*
-             * IMPORTANT NOTE: the input Mat that is passed in as a parameter to this method
-             * will only dereference to the same image for the duration of this particular
-             * invocation of this method. That is, if for some reason you'd like to save a copy
-             * of this particular frame for later use, you will need to either clone it or copy
-             * it to another Mat.
-             */
-
-            /*
-             * Draw a simple box around the middle 1/2 of the entire frame
-             */
 
             Mat outPut = new Mat();
             int Rect1X = 20;
@@ -222,7 +139,6 @@ public class OpenCVWebcamExample extends LinearOpMode
             Mat RightCrop = new Mat();
             //Left location = 0, center location = 1, right location = 2
             int TeamEleLoc = -1;
-
 
             input.copyTo(outPut);
             Rect rect = new Rect(Rect1X, Rect1Y, Rectwidth, Rectheight);
@@ -252,16 +168,16 @@ public class OpenCVWebcamExample extends LinearOpMode
             Scalar RightAverageR = Core.mean(RightCrop);
             double FinalRightAverageR = RightAverageR.val[0];
 
-            if (FinalRightAverageR < 200 && FinalCenterAverageR < 200) {
+            if (FinalRightAverageR < 150 && FinalCenterAverageR < 150) {
                 //team element = left
                 TeamEleLoc = 0;
             }
 
-            if (FinalCenterAverageR > 200){
+            if (FinalCenterAverageR > 150){
                 //team element = center
                 TeamEleLoc = 1;
             }
-            if (FinalRightAverageR > 200){
+            if (FinalRightAverageR > 150){
                 //team element = right
                 TeamEleLoc = 2;
             }
@@ -269,6 +185,7 @@ public class OpenCVWebcamExample extends LinearOpMode
             telemetry.addData("right square green channel", FinalRightAverageR);
             telemetry.addData("Team Element Location", TeamEleLoc);
             telemetry.update();
+            sleep(500);
             return outPut;
 
             /**
@@ -278,7 +195,12 @@ public class OpenCVWebcamExample extends LinearOpMode
              */
 
             //return input;
-        }
+
+
+            // write crop code for third square
+            // write two square method code for comparing two of the squares
+            // white three square method for determinng team element locationat highest value of three squares
+            }
 
         @Override
         public void onViewportTapped()
